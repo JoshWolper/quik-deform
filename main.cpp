e1442b857fb54ebff4eefe57b1441776c0b6d81b
Josh Wolper <joshwolper@gmail.com>
2018-02-21 12:21:42 -0500

    Adding starting files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//Projective Dynamics solver main function
+//Standalone app for QuikDeform Maya plugin
+//Developed by Josh Wolper and Yichen Shou
+
+#include "Eigen/Dense"
+
+int main(){
+	
+
+
+
+	return 0;
+}
